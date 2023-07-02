2b9c553bb9706acc1fb589aee710cc194866b612
JamFox <github@jamfox.dev>
2023-07-03 00:28:29 +0300

    test fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css/styles.css");
+    // copy static assets to the output folder
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/fonts");
+    eleventyConfig.addPassthroughCopy("./src/static");
     return {
+        // specify input and output directories
         dir: {
             input: "src",
             output: "public",
